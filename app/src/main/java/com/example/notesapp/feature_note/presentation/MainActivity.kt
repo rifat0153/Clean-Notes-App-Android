fbb92af4ce0ffe78617c847ac8bb9e48befcda7c
Mahbubur Rahman <rifat0153@gmail.com>
2021-11-21 21:43:47 +0000

    Notescreen UI componets DONE

--- app/src/main/java/com/example/notesapp/feature_note/presentation/MainActivity.kt
+++ app/src/main/java/com/example/notesapp/feature_note/presentation/MainActivity.kt
@@ -3,21 +3,25 @@ package com.example.notesapp.feature_note.presentation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.findNavController
+import com.example.notesapp.feature_note.presentation.notes.components.NotesScreen
 import com.example.notesapp.ui.theme.NotesAppTheme
 
 class MainActivity : ComponentActivity() {
+    @ExperimentalAnimationApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             NotesAppTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.onBackground) {
-                    Greeting("Android")
+                    Greeting(name = "Rifat")
                 }
             }
         }
